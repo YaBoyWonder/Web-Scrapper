3f481a8a2508c47e744a2d05f0b0a298aff93f6b
Rahul Chalumuri <rahulchalumuri54@gmail.com>
2018-08-13 07:38:19 -0700

    Update pokemon.js

--- pokemon.js
+++ pokemon.js
@@ -1,20 +1,25 @@
 const request = require('request');
 const cheerio = require('cheerio');
 
+//pokemon.com/us is the website url. Make sure to change it to whatever website you're looking into
 request('https://www.pokemon.com/us', (error, Response, html) => {
 
         if (!error) {
 
                 const $ = cheerio.load(html)
 
+                //div_classes the info is under
                 const sliderWrapper = $('.slider-wrapper');
                 const episodeTitle = $('.slider-title');
-
+                 
+                
+                //grabbing the header titles of the pokemon names, pokedex no, episodes, and the titles provided
                 const output = episodeTitle.find('h3').text();
                 const output2 = sliderWrapper.find('h5').text()
 
+                
+                //printing into console
                 console.log(output)
-
                 console.log(output2)
 
         }
